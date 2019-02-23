9d089369626c55a9da97e455a1e2d06c99a5b7cc
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-24 00:38:11 +0800

    winner message updated

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -10,7 +10,7 @@ class Board extends Component {
     }
   }
 
-  handleClick(i) {
+  handleClick = (i) => {
     const squares = this.state.squares.slice();
     if (this.props.calculateWinner(squares) || squares[i]) {
       return;
@@ -22,10 +22,11 @@ class Board extends Component {
     });
   }
 
-  renderSquare(i) {
+  renderSquare = (i) => {
     return (
       <Square
         value={this.state.squares[i]}
+        nextPlayer={this.state.nextPlayer}
         onClick={() => this.handleClick(i)}
       />
     );
@@ -33,30 +34,43 @@ class Board extends Component {
 
   render() {
     const winner = this.props.calculateWinner(this.state.squares);
+    const activePlayer = this.state.nextPlayer ? "X" : "O";
     let status;
+    let image;
     if (winner) {
-      status = "Winner: " + winner;
-    } else {
-      status = "Next player: " + (this.state.nextPlayer ? "X" : "O");
+      status = "We have a winner!";
+      image = "../src/winner.png";
+    // } else {
+    //   status = "Player: " + activePlayer;
     }
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
+      <div className="status">
+        <div className={`winner ${winner}`}>{winner}</div>
+        <div className="message">{status}</div>
+        <img src={image} />
+      </div>
+        <div className="players">
+          <div className={`player${this.state.nextPlayer ? " " + activePlayer : ""}`}>Player X</div>
+          <div className={`player${this.state.nextPlayer ? "" : " " + activePlayer}`}>Player O</div>
         </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
+        <div className="board">
+          <div className="row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
         </div>
       </div>
     );
