bdd189274063be4c0868751cdf8f71c889f7e2a3
Joyce Wong <wcyjoyce.hk@gmail.com>
2019-02-24 00:34:11 +0800

    splash content added

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import Board from "./board.jsx";
 import Game from "./game.jsx";
 import Square from "./square.jsx";
 
+const Splash = () => {
+  return (
+    <div>
+      <h1>Tic / Tac / Toe</h1>
+      <button>Start</button>
+      <img src={"../src/winner.png"} className="hidden" />
+    </div>
+  );
+}
+
 class App extends Component {
   render() {
     return (
-      <Game />
+      <div className="flex">
+        <div className="left"><Splash /></div>
+        <div className="right"><Game /></div>
+      </div>
     );
   }
 }
